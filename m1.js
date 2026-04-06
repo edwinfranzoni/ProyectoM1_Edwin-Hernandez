ab2a336a019bfc9d81d1bd34884164e1b44658a9
edwin hernandez franzoni <edwin_wii@hotmail.com>
2026-04-05 19:28:21 -0600

    corregi formatos corregi colores y mande llamar los arrays

--- m1.js
+++ m1.js
@@ -1,19 +1,28 @@
-const coloreshex = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#FFFFFF', '#000000', '#888888'];
-const colorsHSL = ['hsl(0, 100%, 50%)', 'hsl(120, 100%, 50%)', 'hsl(240, 100%, 50%)', 'hsl(60, 100%, 50%)', 'hsl(300, 100%, 50%)', 'hsl(180, 100%, 50%)', 'hsl(0, 0%, 100%)', 'hsl(0, 0%, 0%)', 'hsl(0, 0%, 53%)'];
+const coloreshex = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#FFFFFF', '#974c4c', '#888888'];
+const colorsHSL = ['hsl(0, 100%, 50%)', 'hsl(120, 100%, 50%)', 'hsl(240, 100%, 50%)', 'hsl(60, 100%, 50%)', 'hsl(300, 100%, 50%)', 'hsl(180, 100%, 50%)', 'hsl(0, 0%, 100%)', 'hsl(133, 56%, 35%)', 'hsl(0, 0%, 53%)'];
 
 const contenedor = document.getElementById("contenedor");
 
 function generarColores(cantidad) {
-  contenedor.innerHTML = ""; // limpia antes
+  contenedor.innerHTML = ""; 
+
+  const formato = document.getElementById("formatSelect").value;
 
   for (let i = 0; i < cantidad; i++) {
     const cuadro = document.createElement("div");
     cuadro.classList.add("cuadro");
 
+    let color;
+
     // color aleatorio
-    const color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    if (formato === "HEX") {
+        color = coloreshex[Math.floor(Math.random() * coloreshex.length)];
+        } else {
+        color = colorsHSL[Math.floor(Math.random() * colorsHSL.length)];
+        }
 
     cuadro.style.backgroundColor = color;
+    cuadro.textContent = color;
 
     contenedor.appendChild(cuadro);
   }
